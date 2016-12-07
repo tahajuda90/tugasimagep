b00a5189ee6fefe4271d9d11faa63d1cd12316b0
tahajuda90 <tahajudamandariansah@gmail.com>
2016-12-08 01:08:05 +0700

    add more line
    update for threshold, using graythresh

--- projek/prjpcd.m
+++ projek/prjpcd.m
@@ -4,21 +4,21 @@ b=rgb2gray(I);
 
 
 NewImg=histeq(b);
-hasil=adflt(NewImg);
+hasil1=adflt(NewImg);
+level=graythresh(hasil1);
+coba=im2bw(hasil1,level);
 
  
- 
- 
- %thresholding
- for bar=1:size(hasil,1)
-     for kol=1:size(hasil,2)
-         if hasil(bar,kol) >= mean(max(hasil))
-             coba(bar,kol)=255;
-         else
-             coba(bar,kol)=0;
-         end
-     end
- end
+%  %thresholding
+%  for bar=1:size(hasil,1)
+%      for kol=1:size(hasil,2)
+%          if hasil(bar,kol) >= mean(max(hasil))
+%              coba(bar,kol)=255;
+%          else
+%              coba(bar,kol)=0;
+%          end
+%      end
+%  end
  
  %opening&closing coba
 
